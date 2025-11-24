6d4790af5c30822e94af095f6118076f91a49c2d
as7926946-alt <as7926946@gmail.com>
2025-11-24 20:07:14 +0530

    Arraylist

---
+++ v25/Arraylist.java
@@ -0,0 +1,55 @@
+import java.util.*;
+/*public class Arraylist {
+    public static void main(String []args){
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        ArrayList<String> list1=new ArrayList<String>();
+        ArrayList<Boolean> list2=new ArrayList<Boolean>();
+
+        // to add an element 
+      
+        list.add(2);
+        list.add(9);
+        list.add(7);
+        System.out.println("The created array list is");
+        System.out.println(list);
+
+        // to get an element
+        int element=list.get(2);
+        System.out.println("The choosen element from the list is ");
+        System.out.println(element);
+
+        // to add an element in between 
+        list.add(1,5);
+        System.out.println("The updated list is ");
+        System.out.println(list);
+
+        //to set element at any index
+        list.set(0, 0);
+        System.out.println("The new changed list is");
+        System.out.println(list);
+
+        // to delete an element
+        list.remove(1);
+        System.out.println("The list after deleting an element is");
+        System.out.println(list);
+
+        // to get size of element
+        int size=list.size();
+        System.out.println("The size of array list is");
+        System.out.println(size);
+
+        //to get element of array using loops
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+" ");
+        }
+     
+        System.out.println();
+       }
+
+       // to sort elements
+        list.add(0);
+       Collections.sort(list);
+       System.out.println(list);
+}
+}*/
+
