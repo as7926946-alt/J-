3110e10863eed4b844a518cdd2484832ee930ce0
as7926946-alt <as7926946@gmail.com>
2025-11-24 11:23:27 +0530

    Created a package

---
+++ MyProject/Main.java
@@ -0,0 +1,10 @@
+import bank.account;
+
+public class Main {
+    public static void main(String[]args){
+        account a=new account();
+        a.name="Anshika";
+        System.out.println("Name"+a.name);
+    }
+    
+}
